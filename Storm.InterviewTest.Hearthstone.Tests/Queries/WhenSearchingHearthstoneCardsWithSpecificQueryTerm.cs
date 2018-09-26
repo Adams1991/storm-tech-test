eb24f23a85ba4f2761c329c5b6ef648397691c3d
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-09-26 15:46:23 +0100

    sets up test for type search with lower case query. test failing

--- Storm.InterviewTest.Hearthstone.Tests/Queries/WhenSearchingHearthstoneCardsWithSpecificQueryTerm.cs
+++ Storm.InterviewTest.Hearthstone.Tests/Queries/WhenSearchingHearthstoneCardsWithSpecificQueryTerm.cs
@@ -9,12 +9,15 @@ using Storm.InterviewTest.Hearthstone.Tests.Specification;
 namespace Storm.InterviewTest.Hearthstone.Tests.Queries
 {
 	[Category("Cache")]
-	public class WhenSearchingHearthstoneCardsWithSpecificQueryTerm : HearthstoneCardCacheContext
+	public class WhenSearchingHearthstoneCardsWithNameContainingSpecificQueryTerm : HearthstoneCardCacheContext
 	{
 		protected IEnumerable<ICard> _result;
+        protected IEnumerable<ICard> _result2;
 		protected string query;
+        protected string query2;
+        protected string query3;
 
-		protected override IEnumerable<ICard> Cards()
+        protected override IEnumerable<ICard> Cards()
 		{
 			return new List<ICard>(base.Cards())
 			{
@@ -23,6 +26,7 @@ namespace Storm.InterviewTest.Hearthstone.Tests.Queries
 					minion.Name = "my special card";
 					minion.Faction = FactionTypeOptions.Alliance;
 					minion.Rarity = RarityTypeOptions.Legendary;
+                    minion.PlayerClass = "Minion";
 				})
 			};
 		}
@@ -30,18 +34,27 @@ namespace Storm.InterviewTest.Hearthstone.Tests.Queries
 		protected override void Context()
 		{
 			query = "special";
-		}
+            query2 = "minion";
+        }
 
 		protected override void Because()
 		{
 			_result = _hearthstoneCardCache.Query(new SearchCardsQuery(query));
-		}
+            _result2 = _hearthstoneCardCache.Query(new SearchCardsQuery(query2));
+        }
 
 		[Test]
 		public void ShouldReturnExpectedSearchResults()
 		{
 			_result.Count().ShouldEqual(1);
 			_result.First().Name.ShouldEqual("my special card");
-		}
+
+            _result2.Count().ShouldEqual(3);
+            _result2.First().Type.ToString().ShouldEqual("Minion");
+        }
+		
+
 	}
 }
+
+
