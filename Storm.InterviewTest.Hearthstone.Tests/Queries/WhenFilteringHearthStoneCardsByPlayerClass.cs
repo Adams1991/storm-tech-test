16d12d49864be66302dc08ebe677de888f4a8e84
Shaun Adams <38860831+Adams1991@users.noreply.github.com>
2018-09-27 09:03:50 +0100

    setup filter test. First test passing due to empty string method already present in search function

---
+++ Storm.InterviewTest.Hearthstone.Tests/Queries/WhenFilteringHearthStoneCardsByPlayerClass.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Storm.InterviewTest.Hearthstone.Core.Common.Queries;
+using Storm.InterviewTest.Hearthstone.Core.Features.Cards.Domain;
+using Storm.InterviewTest.Hearthstone.Tests.Base;
+using Storm.InterviewTest.Hearthstone.Tests.Specification;
+
+namespace Storm.InterviewTest.Hearthstone.Tests.Queries
+{
+    class WhenFilteringHearthStoneCardsByPlayerClass : HearthstoneCardCacheContext
+    {
+        protected IEnumerable<ICard> _result;
+        protected string noFilter;
+
+
+        protected override void Context()
+        {
+            noFilter = null;
+        }
+
+
+        protected override void Because()
+        {
+            _result = _hearthstoneCardCache.Query(new SearchCardsQuery(noFilter));
+        }
+
+
+        [Test]
+        public void ShouldReturnExpectedSearchResults()
+        {
+            _result.Count().ShouldEqual(4);
+        }
+    }
+}
